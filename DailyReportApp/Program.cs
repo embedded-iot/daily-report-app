9993849f9d17ca0a5cdbbd36cf8bb61411f8852e
nguyenquan5895 <nguyenquan5895@gmail.com>
2021-09-27 01:22:27 +0700

    Read column names from excell

--- DailyReportApp/Program.cs
+++ DailyReportApp/Program.cs
@@ -11,7 +11,7 @@ namespace DailyReportApp
 {
     static class Program
     {
-        private static string[] columnNames = { "FLOOR", "LONGDATE", "SHORTDATE" };
+        
         private static OracleConnection GetDBConnection()
         {
             string host = "127.0.0.1";
@@ -22,44 +22,59 @@ namespace DailyReportApp
             return DBOracleUtils.GetDBConnection(host, port, sid, user, password);
         }
 
-        private static string[,] QueryAllData(OracleConnection conn)
+        private static string[,] QueryAllData(OracleConnection conn, string[] columnNames)
         {
-            string[,] data;
+            string[,] data = {};
             int rowIndex = 0;
             DbDataReader reader = DBOracleUtils.ExecuseQuery(conn, "SELECT * from na_r_acs_tranfertimejob_vw");
-            using (reader)
+            DbDataReader readerCount = DBOracleUtils.ExecuseQuery(conn, "SELECT COUNT(*) from na_r_acs_tranfertimejob_vw");
+            int countRows = 0;
+            try
             {
+                if (readerCount.HasRows)
+                {
+                    readerCount.Read();
+                    countRows = readerCount.GetInt32(0);
+                }
                 if (reader.HasRows)
                 {
+                    data = new string[countRows, columnNames.Length];
                     while (reader.Read())
                     {
                         for (int index = 0; index < columnNames.Length; index++)
                         {
                             int empNameIndex = reader.GetOrdinal(columnNames[index]);
-                            string empName = reader.GetString(empNameIndex);
+                            string empName = reader.GetValue(empNameIndex).ToString();
                             data[rowIndex, index] = empName;
-                            
-                        }
 
+                        }
+                        rowIndex++;
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                reader.Dispose();
             }
             return data;
         }
 
         private static void CronTabHandler()
         {
+            Console.WriteLine("Generate excel file...");
             OracleConnection conn = GetDBConnection();
+            string path = "F:\\test.xlsx";
             try
             {
                 conn.Open();
-                string[,] data = QueryAllData(conn);
-                string[,] data1 = {
-                                 {"1", "2"},
-                                 {"3", "4"},
-                                 {"5", "6"}
-                             };
-                ExcelUtils.AppendRows("F:\\test.xlsx", data);
+                string[] columnNames = ExcelUtils.ReadRow(path, 1);
+                string[,] data = QueryAllData(conn, columnNames);
+                ExcelUtils.AppendRows(path, data);
             }
             catch (Exception ex)
             {
@@ -71,6 +86,7 @@ namespace DailyReportApp
                 conn.Close();
                 conn.Dispose();
             }
+            Console.WriteLine("Done!");
         }
 
 
--- DailyReportApp/Utils/ExcelUtils.cs
+++ DailyReportApp/Utils/ExcelUtils.cs
@@ -17,8 +17,6 @@ namespace DailyReportApp
             Excel.Range userRange = x.UsedRange;
             int countRecords = userRange.Rows.Count;
             int startIndex = countRecords + 1;
-            Console.WriteLine(rows.GetLength(0));
-            Console.WriteLine(rows.GetLength(1));
             for (int rowIndex = 0; rowIndex < rows.GetLength(0); rowIndex++)
             {
                 for (int columnIndex = 0; columnIndex < rows.GetLength(1); columnIndex++)
@@ -29,6 +27,30 @@ namespace DailyReportApp
                 
             sheet.Close(true, Type.Missing, Type.Missing);
             excel.Quit();
+        }
+
+        public static string[] ReadRow(string path, int rowIndex)
+        {
+            Excel.Application excel = new Excel.Application();
+            Excel.Workbook sheet = excel.Workbooks.Open(path);
+            Excel.Worksheet x = excel.ActiveSheet as Excel.Worksheet;
+            Excel.Range userRange = x.UsedRange;
+            
+            int countColumns = userRange.Columns.Count;
+            string[] data = new string[countColumns];
+            int columnIndex = 1;
+            while (columnIndex <= countColumns)
+            {
+                if (x.Cells[rowIndex, columnIndex] != null && x.Cells[rowIndex, columnIndex].Value2 != null)
+                {
+                    data[columnIndex-1] = x.Cells[rowIndex, columnIndex].Value2.ToString();
+                }
+                columnIndex++;
+            }
+
+            sheet.Close(true, Type.Missing, Type.Missing);
+            excel.Quit();
+            return data;
         }
     }
 }
